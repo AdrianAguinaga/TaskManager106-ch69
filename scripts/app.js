e787c97179a99af81d04df4010773a306226402b
AdrianAguinaga <adrian.aguinaga@uabc.edu.mx>
2026-07-20 19:52:23 -0700

    first commit

---
+++ scripts/app.js
@@ -0,0 +1,22 @@
+
+function saveTask()
+{   
+    //get values from the DOM
+    const title = $("#txtTitle").val();
+    const desc =$("#txtDescription").val();
+    const color =$("#selColor").val();
+    const date =$("#selDate").val();
+    const status =$("#selStatus").val();
+    const budget =$("#numBudget").val();
+    //Create the object
+    const taskToSave = new Task(title, desc, color, date, status, budget);
+    console.log(taskToSave);
+}
+
+function init(){
+    console.log("Hello from 106");
+    $("#btnSave").click(saveTask);
+}
+
+window.onload = init;
+//It force that the HTML and the CSS gets resolved before that the logic gets executed
---
+++ scripts/task.js
@@ -0,0 +1,11 @@
+class Task{
+    constructor(title, description, color, date, status, budget)
+    {
+        this.title = title;
+        this.desc = description; //Note: mapped to desc internally
+        this.color = color;
+        this.date = date;
+        this.status = status;
+        this.budget = budget;
+    }
+}
